44d8202eed096ac484bd26b5a4b9c89040139aea
Jaminima <oscar.davies@hotmail.co.uk>
2018-06-13 22:04:03 +0100

    `X` Closes Correctly

--- FinderSource/NetworkedClient/Program.cs
+++ FinderSource/NetworkedClient/Program.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace NetworkedClient
 {
     static class Program
     {
+        #region "Safe Closing"
+        public enum ConsoleEvent
+        {
+            CTRL_C = 0,
+            CTRL_BREAK = 1,
+            CTRL_CLOSE = 2,
+            CTRL_LOGOFF = 5,
+            CTRL_SHUTDOWN = 6
+        }
+        public delegate void ControlEventHandler(ConsoleEvent consoleEvent);
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ControlEventHandler e, bool add);
+        public static void OnControlEvent(ConsoleEvent consoleEvent)
+        {
+            FinderHandler.Terminate(null,null);
+        }
+        #endregion
         static List<String> LBound = new List<string> { }, UBound = new List<string> { };
         static void Main()
         {
+            SetConsoleCtrlHandler(new ControlEventHandler(OnControlEvent), true);
             NetworkHandler.Start(Handler);
             Console.WriteLine("Running");
             while (true) { Console.ReadLine(); }
