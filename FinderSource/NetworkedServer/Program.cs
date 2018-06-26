5ed771fcc26830369f4ce26082ba252bf8f05264
R-o-b-o <39059788+R-o-b-o@users.noreply.github.com>
2018-06-26 18:26:17 +0100

    Updated Filepaths

--- FinderSource/NetworkedServer/Program.cs
+++ FinderSource/NetworkedServer/Program.cs
@@ -10,6 +10,7 @@ namespace NetworkedServer
 {
     class Program
     {
+        static string FilesLocation = "/var/www/html/NodeJS/";
         static double Combinations = Math.Pow(62, 6),C=Combinations;
         public static int Steps = 0;
         static List<String> Chars = new List<string> {
@@ -95,7 +96,7 @@ namespace NetworkedServer
             {
                 if (wb.DownloadString("https://discordapp.com/api/v6/invite/" + Code + "?with_counts=true").Contains(@", ""guild"": "))
                 {
-                    ValidCodes.Add(Code); System.IO.File.AppendAllText("./ValidCodes.dat", "\nhttps://discord.gg/" + Code);
+                    ValidCodes.Add(Code); System.IO.File.AppendAllText(FilesLocation + "ValidCodes.dat", "\nhttps://discord.gg/" + Code);
                     string GuildID = DiscordAPI.Events.JoinServer(Code);
                     foreach (Newtonsoft.Json.Linq.JObject Room in DiscordAPI.Events.GetTextChannels(GuildID))
                     {
@@ -105,7 +106,7 @@ namespace NetworkedServer
                             try
                             {
                                 string NewInvite = DiscordAPI.Events.CreateInvite((string)Room["id"]);
-                                System.IO.File.WriteAllText("./ValidCodes.dat", System.IO.File.ReadAllText("./ValidCodes.dat").Replace("https://discord.gg/" + Code, "https://discord.gg/" + NewInvite));
+                                System.IO.File.WriteAllText(FilesLocation + "ValidCodes.dat", System.IO.File.ReadAllText(FilesLocation + "ValidCodes.dat").Replace("https://discord.gg/" + Code, "https://discord.gg/" + NewInvite));
                             }
                             catch { Console.WriteLine("Unable To Create/Save Invite"); }
                             break;
@@ -115,7 +116,7 @@ namespace NetworkedServer
                     DiscordAPI.Events.LeaveServer(GuildID);
                     Console.WriteLine("Received Valid Code");
                 }
-                else { System.IO.File.AppendAllText("./Dms.dat", "\nhttps://discord.gg/" + Code); Console.WriteLine("Received Valid Dm"); }
+                else { System.IO.File.AppendAllText(FilesLocation + "Dms.dat", "\nhttps://discord.gg/" + Code); Console.WriteLine("Received Valid Dm"); }
             }
             catch { Console.WriteLine("Received InValid Code"); }
         }
@@ -171,13 +172,13 @@ namespace NetworkedServer
         {
             List<string> oldLines = new List<string>();
             List<string> newLines = new List<string>();
-            oldLines = System.IO.File.ReadAllLines("./ValidCodes.dat").ToList();
+            oldLines = System.IO.File.ReadAllLines(FilesLocation + "ValidCodes.dat").ToList();
 
             foreach (string link in oldLines)
             {
                 if (IsValidCode(link.Replace("https://discord.gg/", ""))) { if (newLines.Contains(link)) { } else { newLines.Add(link); } }
             }
-            System.IO.File.WriteAllLines("./ValidCodes.dat", newLines);
+            System.IO.File.WriteAllLines(FilesLocation + "ValidCodes.dat", newLines);
         }
         static Boolean IsValidCode(string Code)
         {
